ca43b163d54c40cbaee555910408500f69b39246
janaabuturabi <jabamohammad20052019@gmail.com>
2025-05-16 15:59:53 +0300

    Finally

--- process_event.php
+++ process_event.php
@@ -8,6 +8,7 @@ $user = 'evento';
 $pass = '123456';
 $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
+
 try {
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
